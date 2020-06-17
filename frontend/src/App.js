2d8f5de19faca6c7a5f2f29367f59dbda4b3bb83
Stella Tóth-Baranyi <stellasipi@gmail.com>
2020-06-17 22:12:25 +0200

    Create base structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-  render(){
-    return("Hello World")
+  render() {
+    return (
+    <Router>
+      <div className="App">
+      <h4>Ide jön HEADER</h4>
+        <Route exact path="/" render={props => (
+          <React.Fragment>
+            <h4>Ide jön a login</h4>
+          </React.Fragment>
+        )} />
+        <Route path="/details" render={props => (
+          <React.Fragment>
+            <h4>Ide meg a bejelentkezés</h4>
+          </React.Fragment>
+        )} />
+      </div>
+    </Router>
+    )
   };
 }
 
