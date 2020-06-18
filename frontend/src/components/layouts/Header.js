c8432bdef9548457a12f10503882d2a5c11d953c
Stella Tóth-Baranyi <stellasipi@gmail.com>
2020-06-18 17:01:11 +0200

    Fixed header collapse

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
+import "bootstrap/js/src/collapse.js"
 
 class Header extends Component {
 
@@ -43,14 +44,15 @@ class Header extends Component {
             return (
                 <header>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" style={headerStyle} href="/">AuthApp session</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navbar-brand" style={headerStyle} href="/">AuthApp</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="true" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             {this.pageTitles}
                         </div>
+                        <button type="button" className="btn btn-outline-dark">Kijelentkezés</button>
                     </div>
                 </nav>
                 </header>
